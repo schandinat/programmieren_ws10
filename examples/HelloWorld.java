9323f31dde3bf0b16f5f0c0adabe75f89842d4a4
Florian Tobias Schandinat <FlorianSchandinat@gmx.de>
2010-10-21 00:22:38 +0200

    Die erste (unfertige) Version von Folien fuer das erste Tutorium.

---
+++ examples/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * This class implements a little  sample program that prints "Hello World!"
+ */
+class HelloWorld {
+	/**
+	 * This method prints "Hello World!"
+	 *
+	 * @param argv unused 
+	 */
+	public static void main(String argv[]) {
+		System.out.println("Hello World!");
+	}
+}
